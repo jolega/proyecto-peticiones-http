5d45b44ea3f88a6f5e70b1ffdb850c38e302e6be
jolega <alonso_juancho@hotmail.com>
2022-03-22 07:58:01 -0500

    manejo de erores

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
+import { obtenerChiste } from "./js/http-provider";
 
 
-const jokeUrl = 'https://api.chucknorris.io/jokes/random' // enpoint
 
 
-fetch (jokeUrl)
-        .then(resp => resp.json())
-        .then(({id, value}) => {
-            console.log(id, value)
-        });
+
+obtenerChiste().then( console.log);
--- src/js/http-provider.js
+++ src/js/http-provider.js
@@ -1,9 +1,32 @@
 
+
 const jokeUrl = 'https://api.chucknorris.io/jokes/random' // enpoint
 
 
-fetch (jokeUrl)
-        .then(resp => resp.json())
-        .then(({id, value}) => {
-            console.log(id, value)
-        });
+
+const obtenerChiste = async () => {
+    
+    try{
+
+        const resp = await fetch (jokeUrl)
+
+        if(!resp.ok) throw alert('no se puedo realizar la peticion')
+  
+        const chiste = await resp.json();
+  
+        return chiste;
+
+    }catch (err) {
+
+        return{
+            id: 'No se encontro'
+        }
+    }
+
+
+}
+
+
+export{
+    obtenerChiste
+}
