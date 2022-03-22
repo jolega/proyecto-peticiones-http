ebb4c5f990209e8eb7e19907ea4c4fb67d4a86a6
jolega <alonso_juancho@hotmail.com>
2022-03-22 07:42:00 -0500

    leer enpoint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 
-import { saludar } from './js/componentes.js'
-import './styles.css';
 
-const nombre = 'Johan Garcia'
-saludar(nombre)
+const jokeUrl = 'https://api.chucknorris.io/jokes/random' // enpoint
+
+fetch ( jokeUrl ).then( resp => {
+
+    resp.json().then( ({id, value})=> {  // extrae la respuesta  y es una promesa
+        console.log(id);
+        console.log(value);
+    });
+  
+})
