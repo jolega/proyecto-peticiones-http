ba5b2590d917ab7f9128d4aa811027e5732df2f9
jolega <alonso_juancho@hotmail.com>
2022-03-23 23:12:10 -0500

    crud completo

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
 import { init } from "./js/usuarios-page";
 init();
+
+import * as CRUD from "./js/crud-provider";
+
+CRUD.getUsuario(1).then( console.log );
+CRUD.crearUsuario({
+    name: 'Fernando',
+    job:  'Carpintero'
+
+}).then( console.log );
+
+CRUD.actualizarUsuario( 1 , {
+    name: 'Melissa',
+    job:   'Developer'
+}).then( console.log);
+
+CRUD.borrarUsuario (1).then( console.log);
