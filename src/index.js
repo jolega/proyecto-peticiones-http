9280875ed60f1e5495ef6d8f123c937d36582a2d
jolega <alonso_juancho@hotmail.com>
2022-03-23 22:32:40 -0500

    crud

--- src/index.js
+++ src/index.js
@@ -1,9 +1,2 @@
-//import { init } from "./js/chistes-page";
-//import { obtenerUsuarios } from "./js/http-provider";
 import { init } from "./js/usuarios-page";
-
-//init();
-
-//obtenerUsuarios().then( console.log );
-
 init();
--- src/js/usuarios-page.js
+++ src/js/usuarios-page.js
@@ -28,29 +28,13 @@ const crearHtml = () => {
     div.innerHTML = html;
     body.appendChild( div );
 
-    // Crear una referencia al TBODY
-    // ya que los TRs van dentro del tbody
-            // querySelector('tbody');
-            // Crear una variable para mantener la referencia?
-
 }
 
-
-// La función crearFilaUsuario debería de recibir un UNICO usuario
-// con la siguiente estructura
-    // {
-    //     "id": 7,
-    //     "email": "michael.lawson@reqres.in",
-    //     "first_name": "Michael",
-    //     "last_name": "Lawson",
-    //     "avatar": "https://s3.amazonaws.com/uifaces/faces/twitter/follettkyle/128.jpg"
-    // }
 const crearFilaUsuario = ( {email, first_name, last_name, avatar} ) => {
    
     tableBody =document.getElementById("bodyUsuarios")
     item++;
-    // En la tabla deben de colocar un correlativo empezando en 1
-    // También deben de colocar el avatar
+
     const html = `
         <td scope="col"> ${item}. </td>
         <td scope="col">  ${email} </td>
@@ -63,23 +47,15 @@ const crearFilaUsuario = ( {email, first_name, last_name, avatar} ) => {
     tr.innerHTML = html;
     tableBody.append(tr)
 
-    // Añadir el table row (tr) dentro del TBody creado anteriormente
-
 }
 
 
 export const init = async() => {
 
-
-    const usuarios= await obtenerUsuarios();
-    console.log(usuarios);
     crearHtml();
+    const usuarios= await obtenerUsuarios();
     usuarios.forEach(usuario =>  crearFilaUsuario(usuario));
-   
-
-    // Obtener la lista de usuarios usando el servicio creado
-    // Por cada usuario, llamar la función crearFila (for, forEach)
-    // Colocar el init en el index.js, para que se ejecute la creación
+    // puede hacerlo tambien asi  (await obtenerUsuarios()).forEach(usuario => (crearFilaUsuario)
 
 }
 
